e42cce192971b6f20f698a0a9a7d74131c3dac8a
hoiregeno <hoiregeno75@gmail.com>
2026-04-18 21:16:24 +1000

    style: add tailwind styling to Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,16 +6,17 @@ const Card = ({
   },
 }) => {
   return (
-    <div>
-      <h1>{city}</h1>
+    <div className="mt-6 bg-teal-200/10 py-4 px-6 rounded-sm text-center text-teal-300 font-bold">
+      <h1 className="text-3xl ">{city}</h1>
       <img
         src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
         alt={description}
+        className="mx-auto"
       />
 
-      <h2>{Math.round(temp)}°C</h2>
-      <p>{description}</p>
-      <p>Humidity: {humidity}%</p>
+      <h2 className="text-amber-200 text-2xl mb-4">{Math.round(temp)}°C</h2>
+      <p className="capitalize text-lg">{description}</p>
+      <p className="text-lg">Humidity: {humidity}%</p>
     </div>
   );
 };
