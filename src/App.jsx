430147755deaf1c9d00701beedd5d974694d251a
hoiregeno <hoiregeno75@gmail.com>
2026-04-18 20:13:40 +1000

    feat: display error message and render Card with weather data

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,25 @@ import Card from "./components/Card";
 import { useWeather } from "./hooks/useWeather";
 
 const App = () => {
-  return <Card />;
+  const { weather, city, err, fetchWeather, handleChange } = useWeather();
+
+  return (
+    <>
+      <form onSubmit={fetchWeather}>
+        <input
+          type="text"
+          placeholder="Enter city"
+          value={city}
+          onChange={handleChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {err && <p>{err}</p>}
+
+      {weather && <Card weather={weather} />}
+    </>
+  );
 };
 
 export default App;
